2f88cbb0968b2f225f76e6ba98de0aa2458d0f31
Thanh Luan <thanhluando25@gmail.com>
2026-02-04 12:20:40 +0700

    🧪 test(viewmodels): add role and user validator tests

---
+++ test/WarehouseManagement.ViewModels.UnitTest/Systems/Roles/RoleBaseValidatorTest.cs
@@ -0,0 +1,66 @@
+﻿using WarehouseManagement.ViewModels.Systems.Role;
+
+namespace WarehouseManagement.ViewModels.UnitTest.Systems.Roles
+{
+    public class RoleBaseValidatorTest
+    {
+        private readonly RoleBaseValidator<RoleBase> _validator;
+        private readonly RoleBase _request;
+
+        public RoleBaseValidatorTest()
+        {
+            _validator = new RoleBaseValidator<RoleBase>();
+            _request = new RoleBase
+            {
+                Id = "admin",
+                Name = "admin"
+            };
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_Role_Id(string data)
+        {
+            _request.Id = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_Role_Name(string data)
+        {
+            _request.Name = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Request_Role_Empty(string data)
+        {
+            _request.Name = data;
+            _request.Id = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("akdhhahdlhal", "kjakdjkladjladj")]
+        [InlineData("dak", "djhadlah")]
+        [InlineData("kdahdkkjhadkhadjk", "dhalhlkadh")]
+        public void Should_Valid_When_Valid_Input(string name, string id) {
+            _request.Name = name;
+            _request.Id = id;
+
+            var result = _validator.Validate(_request);
+            Assert.True(result.IsValid);
+        }
+    }
+}
---
+++ test/WarehouseManagement.ViewModels.UnitTest/Systems/Users/UserBaseValidatorTest.cs
@@ -0,0 +1,117 @@
+﻿using WarehouseManagement.ViewModels.Systems.User;
+
+namespace WarehouseManagement.ViewModels.UnitTest.Systems.Users
+{
+    public class UserBaseValidatorTest
+    {
+        private UserBaseValidator<UserBase> _validator;
+        private UserBase _request;
+
+        public UserBaseValidatorTest()
+        {
+            _validator = new UserBaseValidator<UserBase>();
+            _request = new UserBase()
+            {
+                UserName = "Test",
+                Email = "example@domain.com",
+                PhoneNumber = "0123456789",
+                FirstName = "Shuhy",
+                LastName = "Daval"
+            };
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_User_Name(string data)
+        {
+            _request.UserName = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_Email(string data)
+        {
+            _request.Email = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_Phone_Number(string data)
+        {
+            _request.PhoneNumber = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_First_Name(string data)
+        {
+            _request.FirstName = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Return_Error_When_Missing_Last_Name(string data)
+        {
+            _request.LastName = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("dkhaljald")]
+        [InlineData("adhkdahl@jdklajl")]
+        [InlineData("adhkdahljdklajl.akdjl")]
+        [InlineData(".akdjl")]
+        [InlineData("@dakla.akdjl")]
+        [InlineData("dakjdalj@kadkad.")]
+        public void Should_Error_When_In_Correct_Format_For_Email(string data)
+        {
+            _request.Email = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);   
+        }
+
+        [Theory]
+        [InlineData("05a4")]
+        [InlineData("445656")]
+        [InlineData("445656+*")]
+        [InlineData("23A*da+")]
+        [InlineData("+84513")]
+        [InlineData("+84513ad")]
+        [InlineData("+84513378#!")]
+        [InlineData("+84513ad+")]
+        public void Should_Error_When_In_Correct_Format_For_Phone_Number(string data)
+        {
+            _request.PhoneNumber = data;
+
+            var result = _validator.Validate(_request);
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Should_Valid_When_Valid_Input()
+        {
+            var result = _validator.Validate(_request);
+            Assert.True(result.IsValid);
+        }
+    }
+}
