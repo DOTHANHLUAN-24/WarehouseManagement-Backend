5b736716951ff635fde20388f7b2303d8de82970
Thanh Luan <thanhluando25@gmail.com>
2026-02-09 10:14:44 +0700

    ✨ feat (controllers): Add CategoriesController API

--- src/WarehouseManagement.BackendServer/Controllers/CategoriesController.cs
+++ src/WarehouseManagement.BackendServer/Controllers/CategoriesController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WarehouseManagement.BackendServer.Data;
+using WarehouseManagement.BackendServer.Data.Entities;
+using WarehouseManagement.ViewModels.Contents.Categories;
+using WarehouseManagement.ViewModels.Systems;
 
 namespace WarehouseManagement.BackendServer.Controllers
 {
@@ -9,11 +13,130 @@ namespace WarehouseManagement.BackendServer.Controllers
     {
 
         [HttpPost]
-        public Task<IActionResult> PostCategory([FromBody] Categ)
+        public async Task<IActionResult> PostCategory([FromBody] CategoryCreateRequest request)
         {
+            var category = new Category
+            {
+                Name = request.Name,
+                ParentId = request.ParentId,
+                SeoAlias = request.SeoAlias,
+                SeoDescription = request.SeoDescription,
+                SortOrder = request.SortOrder,
+            };
+            _context.Categories.Add(category);
+            var result = await _context.SaveChangesAsync();
 
+            if (result > 0)
+                return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+            else
+                return BadRequest();
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+                return NotFound();
 
+            var categoryViewModel = CreateCategoryViewModel(category);
+
+            return Ok(categoryViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = _context.Categories;
+            var categoryViewModels = await categories
+                .Select(category => CreateCategoryViewModel(category)).ToListAsync();
+
+            return Ok(categoryViewModels);
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetCategoriesPaging(string? filter, int pageIndex, int pageSize)
+        {
+            var query = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filter))
+                query = query.Where(x => x.Name.ToLower().Contains(filter));
+
+            var totalRecords = await query.CountAsync();
+
+            var items = await query.Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize).ToListAsync();
+
+            var data = items.Select(category => CreateCategoryViewModel(category)).ToList();
+
+            var pagination = new Pagination<CategoryViewModel>
+            {
+                Items = data,
+                TotalRecords = totalRecords
+            };
+
+            return Ok(pagination);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, [FromBody] CategoryUpdateRequest request)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+                return NotFound();
+
+            if (id == request.ParentId)
+            {
+                return BadRequest();
+            }
+
+            category.Name = request.Name;
+            category.ParentId = request.ParentId;
+            category.SortOrder = request.SortOrder;
+            category.SeoAlias = request.SeoAlias;
+            category.SeoDescription = request.SeoDescription;
+
+
+            _context.Categories.Update(category);
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if(category == null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+
+            var result = await _context.SaveChangesAsync();
+            if(result > 0)
+            {
+                var categoryViewModel = CreateCategoryViewModel(category);
+                return Ok(categoryViewModel);
+            }
+
+            return BadRequest();
+        }
+
+        private static CategoryViewModel CreateCategoryViewModel(Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                ParentId = category.ParentId,
+                SortOrder = category.SortOrder,
+                SeoAlias = category.SeoAlias,
+                SeoDescription = category.SeoDescription
+            };
+        }
     }
 }
