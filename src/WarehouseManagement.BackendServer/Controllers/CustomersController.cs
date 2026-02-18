eb8200d336973f4c9ec23415cdad6b54c45ad4a2
Thanh Luan <thanhluando25@gmail.com>
2026-02-18 23:40:34 +0700

    🐸 feat(View-model): create customer view model

---
+++ src/WarehouseManagement.BackendServer/Controllers/CustomersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseManagement.BackendServer.Data;
+
+namespace WarehouseManagement.BackendServer.Controllers
+{
+    public class CustomersController(ApplicationDbContext _context, ILogger<CustomersController> _logger)
+    {
+    }
+}
