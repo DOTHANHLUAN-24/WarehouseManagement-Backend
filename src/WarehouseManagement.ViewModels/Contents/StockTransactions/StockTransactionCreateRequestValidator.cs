2fc7c5b141812819e3add0fa2943d4ea29b39c1a
Thanh Luan <thanhluando25@gmail.com>
2026-03-14 20:24:42 +0700

    🐣 feat(Stock Transaction): add stock transaction validators

---
+++ src/WarehouseManagement.ViewModels/Contents/StockTransactions/StockTransactionCreateRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace WarehouseManagement.ViewModels.Contents.StockTransactions
+{
+    public class StockTransactionCreateRequestValidator : AbstractValidator<StockTransactionCreateRequest>
+    {
+        public StockTransactionCreateRequestValidator()
+        {
+            Include(new StockTransactionBaseValidator<StockTransactionCreateRequest>());
+        }
+    }
+}
