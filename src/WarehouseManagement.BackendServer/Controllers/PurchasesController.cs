5e68c006f1576b08c10a3e6d02bd9d0f6a94fae7
Thanh Luan <thanhluando25@gmail.com>
2026-03-15 15:02:54 +0700

    🐸 feat(controllers): implement purchase APIs

--- src/WarehouseManagement.BackendServer/Controllers/PurchasesController.cs
+++ src/WarehouseManagement.BackendServer/Controllers/PurchasesController.cs
@@ -1,10 +1,133 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagement.BackendServer.Data;
+using WarehouseManagement.BackendServer.Data.Entities;
+using WarehouseManagement.ViewModels.Contents.Purchases;
 
 namespace WarehouseManagement.BackendServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PurchasesController : ControllerBase
+    public class PurchasesController
+    (
+        ApplicationDbContext _context,
+        ILogger<PurchasesController> _logger
+    ) : ControllerBase
     {
+        [HttpGet]
+        public Task<IActionResult> GetAllPurchase()
+        {
+            var purchases = _context.Purchases
+                .ToList()
+                .Select(x =>
+                new PurchaseViewModel
+                {
+                    Id = x.Id,
+                    SupplierId = x.SupplierId,
+                    PurchaseDate = x.PurchaseDate,
+                    TotalCost = x.TotalCost,
+                    CreateDate = x.CreateDate,
+                    LastModifiedDate = x.LastModifiedDate
+
+                });
+
+            return Task.FromResult<IActionResult>(Ok(purchases));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPurchaseById(int id)
+        {
+            var purchase = await _context.Purchases
+                .Where(x => x.Id == id)
+                .Select(x =>
+                new PurchaseViewModel
+                {
+                    Id = x.Id,
+                    SupplierId = x.SupplierId,
+                    PurchaseDate = x.PurchaseDate,
+                    TotalCost = x.TotalCost,
+                    CreateDate = x.CreateDate,
+                    LastModifiedDate = x.LastModifiedDate
+                })
+                .FirstOrDefaultAsync();
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(purchase);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostPurchase([FromBody] PurchaseCreateRequest request)
+        {
+            var purchase = new Purchase
+            {
+                SupplierId = request.SupplierId,
+                PurchaseDate = request.PurchaseDate,
+                TotalCost = request.TotalCost, // Sử dụng hàm phương thức tính toán ...
+                CreateDate = DateTime.UtcNow
+            };
+
+            _context.Purchases.Add(purchase);
+
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                return CreatedAtAction(nameof(GetPurchaseById), new { id = purchase.Id }, purchase);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePurchase(int id, [FromBody] PurchaseUpdateRequest request)
+        {
+            var purchase = await _context.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+            purchase.SupplierId = request.SupplierId;
+            purchase.PurchaseDate = request.PurchaseDate;
+            purchase.TotalCost = request.TotalCost; // Sử dụng hàm phương thức tính toán ...
+            purchase.LastModifiedDate = DateTime.UtcNow;
+
+            _context.Purchases.Update(purchase);
+
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePurchase(int id)
+        {
+            var purchase = await _context.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return NotFound();
+            }
+            _context.Purchases.Remove(purchase);
+
+            var result = _context.SaveChanges();
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
     }
 }
