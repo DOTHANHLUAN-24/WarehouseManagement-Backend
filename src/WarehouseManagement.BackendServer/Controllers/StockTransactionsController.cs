11034567cf1916378abc7acdb488f397ccbd6433
Thanh Luan <thanhluando25@gmail.com>
2026-03-16 12:32:09 +0700

    📃 feat(stock transactions): integrate Serilog into controller for Backend APIs

--- src/WarehouseManagement.BackendServer/Controllers/StockTransactionsController.cs
+++ src/WarehouseManagement.BackendServer/Controllers/StockTransactionsController.cs
@@ -19,20 +19,13 @@ namespace WarehouseManagement.BackendServer.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllStockTransactions()
         {
+            _logger.LogInformation("Begin GetAllStockTransactions API");
+
             var listStockTransactions = await _context.StockTransactions
-                .Select(x => new StockTransactionViewModel
-                {
-                    Id = x.Id,
-                    ProductId = x.ProductId,
-                    WarehouseId = x.WarehouseId,
-                    QuantityChange = x.QuantityChange,
-                    TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
-                    ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
-                    ReferenceId = x.ReferenceId,
-                    BalanceAfter = x.BalanceAfter,
-                    CreateDate = x.CreateDate,
-                    LastModifiedDate = x.LastModifiedDate
-                }).ToListAsync();
+                .Select(x => CreateStockTransactionViewModel(x))
+                .ToListAsync();
+
+            _logger.LogInformation("GetAllStockTransactions success.");
 
             return Ok(listStockTransactions);
         }
@@ -40,23 +33,20 @@ namespace WarehouseManagement.BackendServer.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetStockTransactionById(int id)
         {
+            _logger.LogInformation("Begin GetStockTransactionById API");
+
             var stockTransaction = await _context.StockTransactions
                 .Where(x => x.Id == id)
-                .Select(x => new StockTransactionViewModel
-                {
-                    Id = x.Id,
-                    ProductId = x.ProductId,
-                    WarehouseId = x.WarehouseId,
-                    QuantityChange = x.QuantityChange,
-                    TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
-                    ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
-                    ReferenceId = x.ReferenceId,
-                    BalanceAfter = x.BalanceAfter,
-                    CreateDate = x.CreateDate,
-                    LastModifiedDate = x.LastModifiedDate
-                }).FirstOrDefaultAsync();
+                .Select(x => CreateStockTransactionViewModel(x))
+                .FirstOrDefaultAsync();
             if (stockTransaction == null)
+            {
+                _logger.LogError("Not found the stock transaction by id = {id}", id);
+
                 return NotFound();
+            }
+
+            _logger.LogInformation("GetStockTransactionById success, id = {id}", id);
 
             return Ok(stockTransaction);
         }
@@ -64,6 +54,8 @@ namespace WarehouseManagement.BackendServer.Controllers
         [HttpPost]
         public async Task<IActionResult> PostStockTransaction(StockTransactionCreateRequest request)
         {
+            _logger.LogInformation("Begin PostStockTransaction API");
+
             var stockTransaction = new StockTransaction
             {
                 ProductId = request.ProductId,
@@ -82,7 +74,14 @@ namespace WarehouseManagement.BackendServer.Controllers
             var result = await _context.SaveChangesAsync();
 
             if (result > 0)
+            {
+                _logger.LogInformation("PostStockTransaction success with id = {id}", stockTransaction.Id);
+
                 return CreatedAtAction(nameof(GetStockTransactionById), new { id = stockTransaction.Id }, stockTransaction);
+            }
+
+            _logger.LogWarning("PostStockTransaction failed to save changes");
+
             return BadRequest();
         }
 
@@ -95,6 +94,10 @@ namespace WarehouseManagement.BackendServer.Controllers
             int pageSize = 10
         )
         {
+            _logger.LogInformation("Begin GetStockTransactions API with " +
+                "product name = {productName}, warehouseEmail = {warehouseEmail}, pageIndex = {pageIndex}, pageSize = {pageSize}", 
+                productName, warehouseEmail, pageIndex, pageSize);
+
             pageIndex = pageIndex <= 0 ? 1 : pageIndex;
 
             pageSize = pageSize <= 0 ? 10 : pageSize;
@@ -104,6 +107,8 @@ namespace WarehouseManagement.BackendServer.Controllers
             // Filter by product name
             if (!string.IsNullOrEmpty(productName))
             {
+                _logger.LogInformation("Begin get stock transactions by the product name = {productName}", productName);
+
                 var productIds = await _context.Products
                     .Where(p => p.Name.Contains(productName))
                     .Select(p => p.Id)
@@ -115,6 +120,8 @@ namespace WarehouseManagement.BackendServer.Controllers
             // Filter by warehouse name
             if (!string.IsNullOrEmpty(warehouseEmail))
             {
+                _logger.LogInformation("Begin get stock transactions by the warehouse email = {warehouseEmail}", warehouseEmail);
+
                 var warehouseIds = await _context.Warehouses
                     .Where(w => w.Email.Contains(warehouseEmail))
                     .Select(w => w.Id)
@@ -129,19 +136,8 @@ namespace WarehouseManagement.BackendServer.Controllers
                 .OrderByDescending(x => x.Id)
                 .Skip((pageIndex - 1) * pageSize)
                 .Take(pageSize)
-                .Select(x => new StockTransactionViewModel
-                {
-                    Id = x.Id,
-                    ProductId = x.ProductId,
-                    WarehouseId = x.WarehouseId,
-                    QuantityChange = x.QuantityChange,
-                    TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
-                    ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
-                    ReferenceId = x.ReferenceId,
-                    BalanceAfter = x.BalanceAfter,
-                    CreateDate = x.CreateDate,
-                    LastModifiedDate = x.LastModifiedDate
-                }).ToListAsync();
+                .Select(x => CreateStockTransactionViewModel(x))
+                .ToListAsync();
 
             var result = new Pagination<StockTransactionViewModel>
             {
@@ -149,27 +145,22 @@ namespace WarehouseManagement.BackendServer.Controllers
                 TotalRecords = totalRecords
             };
 
+            _logger.LogInformation("GetStockTransactions success. Total records = {TotalRecords}, items = {items}", totalRecords, items);
+
             return Ok(result);
         }
 
         [HttpGet("warehouse/{warehouseId}")]
         public async Task<IActionResult> GetStockTransactionsByWarehouseId(int warehouseId)
         {
+            _logger.LogInformation("Begin GetStockTransactionsByWarehouseId API");
+
             var stockTransactions = await _context.StockTransactions
                 .Where(x => x.WarehouseId == warehouseId)
-                .Select(x => new StockTransactionViewModel
-                {
-                    Id = x.Id,
-                    ProductId = x.ProductId,
-                    WarehouseId = x.WarehouseId,
-                    QuantityChange = x.QuantityChange,
-                    TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
-                    ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
-                    ReferenceId = x.ReferenceId,
-                    BalanceAfter = x.BalanceAfter,
-                    CreateDate = x.CreateDate,
-                    LastModifiedDate = x.LastModifiedDate
-                }).ToListAsync();
+                .Select(x => CreateStockTransactionViewModel(x))
+                .ToListAsync();
+
+            _logger.LogInformation("GetStockTransactionsByWarehouseId success");
 
             return Ok(stockTransactions);
         }
@@ -177,23 +168,33 @@ namespace WarehouseManagement.BackendServer.Controllers
         [HttpGet("stockTransactions/reference/{referenceType}/{referenceId}")]
         public async Task<IActionResult> GetStockTransactionsByReference(ReferenceType referenceType, int referenceId)
         {
+            _logger.LogInformation("Begin GetStockTransactionsByReference API");
+
             var stockTransactions = await _context.StockTransactions
                 .Where(x => x.ReferenceType == referenceType && x.ReferenceId == referenceId)
-                .Select(x => new StockTransactionViewModel
-                {
-                    Id = x.Id,
-                    ProductId = x.ProductId,
-                    WarehouseId = x.WarehouseId,
-                    QuantityChange = x.QuantityChange,
-                    TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
-                    ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
-                    ReferenceId = x.ReferenceId,
-                    BalanceAfter = x.BalanceAfter,
-                    CreateDate = x.CreateDate,
-                    LastModifiedDate = x.LastModifiedDate
-                }).ToListAsync();
+                .Select(x => CreateStockTransactionViewModel(x))
+                .ToListAsync();
+
+            _logger.LogInformation("GetStockTransactionsByReference success");
 
             return Ok(stockTransactions);
+        }
+
+        private static StockTransactionViewModel CreateStockTransactionViewModel(StockTransaction x)
+        {
+            return new StockTransactionViewModel
+            {
+                Id = x.Id,
+                ProductId = x.ProductId,
+                WarehouseId = x.WarehouseId,
+                QuantityChange = x.QuantityChange,
+                TransactionType = (ViewModels.Enums.StockTransactionType)x.TransactionType,
+                ReferenceType = (ViewModels.Enums.ReferenceType)x.ReferenceType,
+                ReferenceId = x.ReferenceId,
+                BalanceAfter = x.BalanceAfter,
+                CreateDate = x.CreateDate,
+                LastModifiedDate = x.LastModifiedDate
+            };
         }
     }
 }
