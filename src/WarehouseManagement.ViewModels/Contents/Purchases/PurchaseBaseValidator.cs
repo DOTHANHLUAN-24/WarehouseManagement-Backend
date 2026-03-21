dac620b82d9feb23ccf060bba130f55a8de7641e
Thanh Luan <thanhluando25@gmail.com>
2026-03-21 09:11:52 +0700

    🔗 feat(purchase): add purchase validator

---
+++ src/WarehouseManagement.ViewModels/Contents/Purchases/PurchaseBaseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace WarehouseManagement.ViewModels.Contents.Purchases
+{
+    public class PurchaseBaseValidator<T> : AbstractValidator<T>
+        where T : PurchaseBase
+    {
+        public PurchaseBaseValidator()
+        {
+            RuleFor(x => x.SupplierId)
+                .NotEmpty().WithMessage("Supplier id is required");
+
+            RuleFor(x => x.TotalCost)
+                .GreaterThanOrEqualTo(0).WithMessage("Total cost is must be greater or equal than 0")
+                .PrecisionScale(18, 2, true).WithMessage("Total cost must be decimal(18, 2)");
+        }
+    }
+}
---
+++ src/WarehouseManagement.ViewModels/Contents/Purchases/PurchaseCreateRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace WarehouseManagement.ViewModels.Contents.Purchases
+{
+    public class PurchaseCreateRequestValidator :AbstractValidator<PurchaseCreateRequest>
+    {
+        public PurchaseCreateRequestValidator()
+        {
+            Include(new PurchaseBaseValidator<PurchaseCreateRequest>());
+        }
+    }
+}
