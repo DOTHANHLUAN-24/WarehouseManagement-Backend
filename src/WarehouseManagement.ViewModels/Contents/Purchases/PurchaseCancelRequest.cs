56ac5f65ddcba6c899bf693d1a0cc4e3504b7666
Thanh Luan <thanhluando25@gmail.com>
2026-06-11 23:14:25 +0700

    feat: implement PurchasesController and request models for purchase management in BackendServer

--- src/WarehouseManagement.ViewModels/Contents/Purchases/PurchaseCancelRequest.cs
+++ src/WarehouseManagement.ViewModels/Contents/Purchases/PurchaseCancelRequest.cs
@@ -3,5 +3,6 @@ namespace WarehouseManagement.ViewModels.Contents.Purchases
     public class PurchaseCancelRequest
     {
         public string? cancelReason { get; set; }
+        public string? noteCancel { get; set; }
     }
 }
